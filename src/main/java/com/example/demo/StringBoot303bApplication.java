dd6ca35f4746caa5ad11a703e9ce2fdd908259cc
prasantib <prasanti.babu@gmail.com>
2019-10-09 15:01:55 -0400

    StringBoot_303b

---
+++ src/main/java/com/example/demo/StringBoot303bApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot303bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot303bApplication.class, args);
+    }
+
+}
